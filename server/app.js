dc2805bc66cbcc2ab239cea7e5a6b279674e2832
sinanananaa <sinanovica1@gmail.com>
2023-01-12 21:41:08 +0100

    Init node and react app

---
+++ server/app.js
@@ -0,0 +1,12 @@
+"use strict";
+const express = require('express'),
+    app = express(),
+    port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
